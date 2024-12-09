9c90a71f3f59fd72b98cc87ba9a0449c7f712c9d
Ronnz98 <56398486+Ronnz98@users.noreply.github.com>
2024-12-09 11:27:33 +0100

    Update AndroidProducts.mk
    support lineage again

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_z3s.mk
+    $(LOCAL_DIR)/lineage_z3s.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_z3s-user \
-    derp_z3s-userdebug \
-    derp_z3s-eng
+    lineage_z3s-user \
+    lineage_z3s-userdebug \
+    lineage_z3s-eng
